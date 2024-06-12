d3b5c971ca88ba8ab7797955b3465e6ffa651d9c
Lisardgy <key.cmdark@gmail.com>
2024-06-12 09:51:48 +0700

    feat: setup nextjs & nextUI

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button, Link } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Wine1o1 website
+      <Link href="https://dope-stuff.github.io/wine101-landing-page">
+        <Button>next button</Button>
+      </Link>
+    </main>
+  );
+}
