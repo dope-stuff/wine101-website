c7de1a18a22f29440c102a649d76582ad4dd89fd
DOPE Engineers <engineers.dope@gmail.com>
2024-06-29 11:23:38 +0700

    fix: workshop card

--- src/modules/card/template/workshop/index.tsx
+++ src/modules/card/template/workshop/index.tsx
@@ -9,17 +9,17 @@ export default function WorkShopCard({data}: WorkShopCardProps) {
   const {name, description, event_date, posters} = data
   return (
     <div className="min-w-[240px] flex-col flex gap-2 p-2 border border-[#CFCFCF]">
-      <div className="relative w-full h-[150px] bg-primary">
+      <div className="relative w-full h-[200px] bg-primary">
         <Image
-          className="w-full max-h-[180px] object-cover !rounded-none"
+          className="w-full max-h-[200px] object-cover !rounded-none"
           removeWrapper
           src={posters}
           alt=""
         />
       </div>
-      <div className="text-[14px]">
+      <div className="text-[14px] gap-2">
         <div className="text-lg]">{name}</div>
-        <div>{description}</div>
+        <strong className="font-bold text-[#BE1C2D]">{description}</strong>
         <div>{dayjs(event_date).format('DD MMMM YYYY')}</div>
       </div>
       <div className="flex-row flex flex-nowrap gap-2 justify-end mt-4">
