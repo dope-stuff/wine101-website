fd3d93ed2a4c2d0e9538fd4b1d4340f7a8a2ea8f
DOPE Engineers <engineers.dope@gmail.com>
2024-07-04 15:06:04 +0700

    fix: remove .env & add api token

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,5 +1,11 @@
 import Axios from 'axios'
 
 const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+const NEXT_API_TOKEN = process.env.NEXT_API_TOKEN
 
-export const axios = Axios.create({baseURL: `${NEXT_PUBLIC_BACKEND_URL}/api`})
+export const axios = Axios.create({
+    baseURL: `${NEXT_PUBLIC_BACKEND_URL}/api`,
+    headers: {
+        Authorization: `Bearer ${NEXT_API_TOKEN}`
+    }
+})
