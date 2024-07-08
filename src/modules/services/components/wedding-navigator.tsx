a76c4c0c6938c4c212773af62795d7d092b2b4c7
Lisardgy <key.cmdark@gmail.com>
2024-07-08 12:57:34 +0700

    feat: update feedback

--- src/modules/services/components/wedding-navigator.tsx
+++ src/modules/services/components/wedding-navigator.tsx
@@ -1,20 +1,36 @@
+'use client'
 import {Link} from '@nextui-org/react'
+import {useRouter} from 'next/navigation'
 
 export default function WeddingNavigator() {
+  const router = useRouter()
+
   const panels = [
     {title: '101 wedding guide', href: 'guide'},
-    {title: ''},
+    {title: '',href: ''},
     {title: 'our package', href: 'package'},
-    {title: ''},
+    {title: '',href: ''},
     {title: 'quick consult', href: 'consult'},
-    {title: ''},
+    {title: '',href: ''},
     {title: 'our client’s review', href: 'reviews'},
   ]
+
+  const handleClick = (href:string) => {
+    router.push(`#${href}`)
+  }
+
   return panels.map((panel, i) =>
     i % 2 !== 0 ? (
       i !== panels.length - 1 && <div key={i} className="w-[2px] h-6 bg-black" />
     ) : (
-      <Link key={i} href={`/wedding/#${panel.href}`}>
+      <Link
+        key={i}
+        href={`#${panel.href}`}
+        onClick={(e) => {
+          e.preventDefault()
+          handleClick(panel.href)
+        }}
+      >
         {panel.title}
       </Link>
     )
