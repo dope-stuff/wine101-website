903670a3f2ef48200757438df8dca4152ea56ae6
Lisardgy <key.cmdark@gmail.com>
2024-07-09 16:22:51 +0700

    feat: add video to pop-up

--- src/app/(service)/pop-up/page.tsx
+++ src/app/(service)/pop-up/page.tsx
@@ -8,6 +8,7 @@ import PopUpBoothButton from '@/modules/button/components/pop-up-booth'
 import Carousel from '@/modules/carousel/template'
 import ClientCard from '@/modules/card/template/client'
 import {eventService} from '@/lib/data/event.service'
+import SmartPhoneImage from '@/modules/common/images/smart-phone'
 
 export default async function Page() {
   const [{data: pageData}, {data: events}] = await Promise.all([
@@ -35,17 +36,37 @@ export default async function Page() {
         gap={0}
       />
       <div className="max-w-[2040px] w-full justify-center flex-col flex items-center mx-auto">
-        <div className="relative flex-row flex w-full justify-center">
-          <FlasksIcon width={100} height={100} className="hidden md:flex absolute right-20 top-20" />
-          <div className="max-w-[90%] md:max-w-[60%] w-full flex-col flex text-center items-center justify-center mt-8 relative gap-4">
-            <div className="text-2xl md:text-3xl font-semibold uppercase">
-              {pageData.header.heading}
+        <div className="relative flex-row flex w-full justify-center"></div>
+        <div className="w-full h-full flex-col lg:flex-row flex lg:items-start items-center justify-center mt-8 gap-8">
+          <div className="w-full lg:max-w-[60%] lg:h-full flex-col flex items-center text-3xl text-center gap-4 relative">
+            <FlasksIcon
+              width={100}
+              height={100}
+              className="hidden md:flex absolute right-4 lg:right-10 top-20"
+            />
+            <div className="max-w-[90%] md:max-w-[60%] w-full flex-col flex text-center items-center justify-center mt-8 relative gap-4">
+              <div className="text-2xl md:text-3xl font-semibold uppercase">
+                {pageData.header.heading}
+              </div>
+              <div
+                className={`max-w-xl lg:max-w-2xl xl:max-w-none text-2xl md:text-3xl ${iannDog.className}`}
+              >
+                {pageData.header.subheading}
+              </div>
             </div>
-            <div
-              className={`max-w-xl lg:max-w-2xl xl:max-w-none text-2xl md:text-3xl ${iannDog.className}`}
-            >
-              {pageData.header.subheading}
-            </div>
+          </div>
+          <div className="relative">
+            <SmartPhoneImage className="max-w-[250px] flex-1 h-full" />
+            {pageData.header && (
+              <iframe
+                className="!absolute top-[5px] left-[8px] w-[233px] h-[98%] rounded-[2rem] z-10"
+                src={pageData.header.mediaUrl}
+                title={pageData.header.alt}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              />
+            )}
           </div>
         </div>
         <div className="w-[90%] flex-col flex mt-8 gap-4">
